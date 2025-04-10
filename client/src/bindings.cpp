4cfcd2f44049792255ade45775af7d0eeb4c3d46
Leonidas810 <leonardo20010912@gmail.com>
2025-04-10 07:15:22 -0600

    update: deleting unnecessary files

--- client/src/bindings.cpp
+++ client/src/bindings.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <pybind11/functional.h> // Necesario para std::function
+#include <pybind11/functional.h>
 #include "client.h"
 #include "terminal_ui.h"
 
